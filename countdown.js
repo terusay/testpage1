700c9089c388689bfbdafb1a4b79e5856f24db3b
terusay <63452176+terusay@users.noreply.github.com>
2021-10-16 22:51:13 +0900

    Update countdown.js

--- countdown.js
+++ countdown.js
@@ -1,1 +1,15 @@
 'use strict';
+
+function countdown(due){
+  const now = new Date();
+  const rest = due.getTime() - now.getTime();
+  const sec = Math.floor(rest/1000);
+  return sec;
+}
+
+let goal = new Date();
+goal.setHours(23);
+goal.setMinutes(59);
+goal.setSeconds(59);
+
+console.log(countdown(goal));
