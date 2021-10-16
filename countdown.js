e082c256e8627529836c2603f4526a2ade453ed4
terusay <63452176+terusay@users.noreply.github.com>
2021-10-16 23:04:05 +0900

    Update countdown.js

--- countdown.js
+++ countdown.js
@@ -2,20 +2,20 @@
 
 function countdown(due){
   const now = new Date();
+  // 以下を編集
   const rest = due.getTime() - now.getTime();
-  const sec = Math.floor(rest/1000);
-  return sec;
+  const sec = Math.floor(rest/1000) % 60;
+  const min = Math.floor(rest/1000/60) % 60;
+  const hours = Math.floor(rest/1000/60/60) % 24;
+  const count = [hours, min, sec];
+
+  return count;
+  // ここまで
 }
 
 let goal = new Date();
-// さっき追加した行
-console.log(goal)
-// ここまで
 goal.setHours(23);
 goal.setMinutes(59);
 goal.setSeconds(59);
-// さらに追加
-console.log(goal)
-// ここまで
 
 console.log(countdown(goal));
